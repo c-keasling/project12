1b145b987d2f2b73eb12579cc653bb3d9783c937
Cody-Keasling-LAS1DA <132222567+Cody-Keasling-LAS1DA@users.noreply.github.com>
2023-10-15 18:18:01 -0700

    initial add

---
+++ script.js
@@ -0,0 +1,45 @@
+const questions = {
+  q1: "Which team was the first to win the Super Bowl?",
+  a1: "Green Bay Packers",
+  q2: "Which team won the most Super Bowls?",
+  a2: "Pittsburgh Steelers",
+  q3: "What team won the most recent Super Bowl?",
+  q4: "Kansas City Cheifs",
+  q4: "What team lost the most Super Bowls?",
+  q5: "Buffalo Bills",
+  q5: "What team never appeared in a Super Bowl?",
+  a5: "Cleveland Browns",
+  q6: "Which team scored the most points in a Super Bowl",
+  a6: "San Francisco 49ers",
+  q7: "Which team scored the least points in a Super Bowl",
+  a7: "Miami Dolphins",
+};
+
+const fillerTeams = [
+  "L.A. Rams",
+  "New York Jets",
+  "Arizona Cardinals",
+  "Chicago Bears",
+  "Minnisota Vikings",
+  "Dallas Cowboys",
+  "Philadelphia Eagles",
+  "Tennese Titans",
+  "San Diego Chargers",
+  "New York Giants",
+  "Atlanta Falcons",
+  "Carolina Panthers",
+  "New Orleans Saints",
+  "New York Giants",
+  "Seattle Seahawks",
+  "Tampa Bay Buccaneers",
+  "Baltimore Ravens",
+  "Cincinnati Bengals",
+  "Denver Broncos",
+  "Raiders",
+];
+
+//Label all the buttons
+const startBtn = document.querySelector(".start-btn");
+const submitBTn = document.querySelector(".submit-btn");
+
+const isPlaying = true;
