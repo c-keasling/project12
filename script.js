bccfebdd65cf6d5e9bfc82cf04e63274cb87d0fc
Cody-Keasling-LAS1DA <132222567+Cody-Keasling-LAS1DA@users.noreply.github.com>
2023-10-16 21:47:06 -0700

    Finished project

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 
 const questions = [
   {
-    q: "Which team was the first to win the Super Bowl?",
+    q: "Which team was the first to win the Super Bowl? 🏈",
     a: "Green Bay Packers",
   },
-  { q: "Which team won the most Super Bowls?", a: "Pittsburgh Steelers" },
-  { q: "What team won the most recent Super Bowl?", a: "Kansas City Cheifs" },
-  { q: "What team lost the most Super Bowls?", a: "Buffalo Bills" },
-  { q: "What team never appeared in a Super Bowl?", a: "Cleveland Browns" },
+  { q: "Which team won the most Super Bowls? 🏆", a: "Pittsburgh Steelers" },
   {
-    q: "Which team scored the most points in a Super Bowl?",
+    q: "What team won the most recent Super Bowl?🙌🙌",
+    a: "Kansas City Cheifs",
+  },
+  { q: "What team lost the most Super Bowls?🤬", a: "Buffalo Bills" },
+  { q: "What team never appeared in a Super Bowl?👻", a: "Cleveland Browns" },
+  {
+    q: "Which team scored the most points in a Super Bowl?📈",
     a: "San Francisco 49ers",
   },
   {
-    q: "Which team scored the least points in a Super Bowl?",
+    q: "Which team scored the least points in a Super Bowl?😶",
     a: "Miami Dolphins",
   },
 ];
@@ -43,6 +46,8 @@ const fillerTeams = [
 ];
 
 //Label all the buttons
+const answersContainer = document.querySelector(".answers-choice");
+const submitBtn = document.querySelector(".submit-btn");
 const startBtn = document.querySelector(".start-btn");
 const submitBTn = document.querySelector(".submit-btn");
 const scoreBox = document.querySelector(".score");
@@ -51,31 +56,48 @@ const answerBTN1 = document.querySelector(".one-box");
 const answerBTN2 = document.querySelector(".two-box");
 const answerBTN3 = document.querySelector(".three-box");
 const answerBTN4 = document.querySelector(".four-box");
-let Q = 0;
-let asking = questions[Q].q;
-let answer = questions[Q].a;
 let answerkey = Math.floor(Math.random() * 4) + 1;
 const buttonSelectors = [".one-box", ".two-box", ".three-box", ".four-box"];
-let playersChoice, buttons;
+let playersChoice,
+  buttons,
+  Qnum = 0,
+  playerScore;
+let asking = questions[Qnum].q;
+let answer = questions[Qnum].a;
 
 const generateAnswers = function () {
+  asking = questions[Qnum].q;
+  answer = questions[Qnum].a;
+  //put random teams on all the buttons
   for (let i = 0; i <= 3; i++) {
     document.querySelector(buttonSelectors[i]).textContent =
       fillerTeams[Math.floor(Math.random() * 20)];
   }
+  //put correct answer on one of the buttons at random
   answerkey = Math.floor(Math.random() * 4);
   document.querySelector(buttonSelectors[answerkey]).textContent = answer;
+  //change question
+  questionElmt.textContent = asking;
   console.log(answer);
 };
-
+// intitiate game
 const initgame = function () {
-  scoreBox.textContent = "0";
+  playerScore = 0;
+  Qnum = 0;
+  submitBtn.textContent = "Submit";
+  scoreBox.textContent = playerScore;
   questionElmt.textContent = asking;
+  generateAnswers();
+  startBtn.innerHTML = "Restart";
+  submitBTn.disabled = false;
+  document.body.classList.remove("winner");
 };
 
 const checkAnswer = function () {
-  if ((answerkey = playerChoice)) {
+  if (answerkey === playersChoice) {
     document.querySelector(buttonSelectors[answerkey]).classList.add("correct");
+    playerScore++;
+    scoreBox.textContent = playerScore;
   } else {
     document.querySelector(buttonSelectors[answerkey]).classList.add("wrong");
   }
@@ -84,6 +106,8 @@ function removeHighlight() {
   if (buttons) {
     buttons.forEach((button) => {
       button.classList.remove("highlighted");
+      button.classList.remove("wrong");
+      button.classList.remove("correct");
     });
   }
 }
@@ -93,14 +117,47 @@ document.addEventListener("DOMContentLoaded", (event) => {
   function handleButtonClick(event) {
     removeHighlight();
     event.target.classList.add("highlighted");
-    playerChoice = [...buttons].indexOf(event.target);
-    console.log("Player's choice is: ", playerChoice);
+    playersChoice = [...buttons].indexOf(event.target);
+    console.log("Player's choice is: ", playersChoice);
   }
 
   buttons.forEach((button) => {
     button.addEventListener("click", handleButtonClick);
   });
 });
-document.generateAnswers();
-initgame();
+
+submitBtn.addEventListener("click", function () {
+  checkAnswer();
+  submitBtn.disabled = true;
+
+  // Set a delay for the next part of the operation.
+  setTimeout(function () {
+    // Increase the question number and prepare for the next round if there are questions left.
+    Qnum++;
+    if (Qnum < 7) {
+      asking = questions[Qnum].q;
+      answer = questions[Qnum].a;
+      generateAnswers();
+      removeHighlight();
+      console.log(`Q= ${Qnum}`);
+      submitBTn.disabled = false;
+    } else {
+      if (playerScore === 7) {
+        questionElmt.textContent = " 🎆 Touchdown! Great Score! 🎉";
+        document.body.classList.add("winner");
+      } else {
+        questionElmt.textContent = "Game over. Good job!";
+      }
+      removeHighlight();
+      if (Qnum === 7) {
+        for (let i = 0; i <= 3; i++) {
+          document.querySelector(buttonSelectors[i]).textContent = "";
+          submitBtn.textContent = "";
+        }
+      }
+    }
+  }, 2000);
+});
+
+startBtn.addEventListener("click", initgame);
 const isPlaying = true;
