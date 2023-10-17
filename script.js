b8d99078cc4ade72254301b81d2e0345bbca0fb8
Cody-Keasling-LAS1DA <cody.keasling@gmail.com>
2023-10-16 17:34:13 -0700

    working on game

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const questions = {
-  q1: "Which team was the first to win the Super Bowl?",
-  a1: "Green Bay Packers",
-  q2: "Which team won the most Super Bowls?",
-  a2: "Pittsburgh Steelers",
-  q3: "What team won the most recent Super Bowl?",
-  q4: "Kansas City Cheifs",
-  q4: "What team lost the most Super Bowls?",
-  q5: "Buffalo Bills",
-  q5: "What team never appeared in a Super Bowl?",
-  a5: "Cleveland Browns",
-  q6: "Which team scored the most points in a Super Bowl",
-  a6: "San Francisco 49ers",
-  q7: "Which team scored the least points in a Super Bowl",
-  a7: "Miami Dolphins",
-};
+"use strict";
+
+const questions = [
+  {
+    q: "Which team was the first to win the Super Bowl?",
+    a: "Green Bay Packers",
+  },
+  { q: "Which team won the most Super Bowls?", a: "Pittsburgh Steelers" },
+  { q: "What team won the most recent Super Bowl?", a: "Kansas City Cheifs" },
+  { q: "What team lost the most Super Bowls?", a: "Buffalo Bills" },
+  { q: "What team never appeared in a Super Bowl?", a: "Cleveland Browns" },
+  {
+    q: "Which team scored the most points in a Super Bowl?",
+    a: "San Francisco 49ers",
+  },
+  {
+    q: "Which team scored the least points in a Super Bowl?",
+    a: "Miami Dolphins",
+  },
+];
 
 const fillerTeams = [
   "L.A. Rams",
@@ -41,5 +45,62 @@ const fillerTeams = [
 //Label all the buttons
 const startBtn = document.querySelector(".start-btn");
 const submitBTn = document.querySelector(".submit-btn");
+const scoreBox = document.querySelector(".score");
+const questionElmt = document.querySelector(".question");
+const answerBTN1 = document.querySelector(".one-box");
+const answerBTN2 = document.querySelector(".two-box");
+const answerBTN3 = document.querySelector(".three-box");
+const answerBTN4 = document.querySelector(".four-box");
+let Q = 0;
+let asking = questions[Q].q;
+let answer = questions[Q].a;
+let answerkey = Math.floor(Math.random() * 4) + 1;
+const buttonSelectors = [".one-box", ".two-box", ".three-box", ".four-box"];
+let playersChoice, buttons;
 
+const generateAnswers = function () {
+  for (let i = 0; i <= 3; i++) {
+    document.querySelector(buttonSelectors[i]).textContent =
+      fillerTeams[Math.floor(Math.random() * 20)];
+  }
+  answerkey = Math.floor(Math.random() * 4);
+  document.querySelector(buttonSelectors[answerkey]).textContent = answer;
+  console.log(answer);
+};
+
+const initgame = function () {
+  scoreBox.textContent = "0";
+  questionElmt.textContent = asking;
+};
+
+const checkAnswer = function () {
+  if ((answerkey = playerChoice)) {
+    document.querySelector(buttonSelectors[answerkey]).classList.add("correct");
+  } else {
+    document.querySelector(buttonSelectors[answerkey]).classList.add("wrong");
+  }
+};
+function removeHighlight() {
+  if (buttons) {
+    buttons.forEach((button) => {
+      button.classList.remove("highlighted");
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  buttons = document.querySelectorAll(".answer");
+  function handleButtonClick(event) {
+    removeHighlight();
+    event.target.classList.add("highlighted");
+    playerChoice = [...buttons].indexOf(event.target);
+    console.log("Player's choice is: ", playerChoice);
+  }
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleButtonClick);
+  });
+});
+document.generateAnswers();
+initgame();
 const isPlaying = true;
